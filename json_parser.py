55ee379e01a6ab9a0ea341a97747b03d31bc5a4b
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-04 19:41:31 +0000

    parse the json data

--- json_parser.py
+++ json_parser.py
@@ -1,31 +1,66 @@
-import re
+from json_lexer import tokens
 
-def tokenize_json(json_string):
+def parse_object(tokens):
+    obj = {}
+    
+    token = next(tokens, None)
+    if token is None or token.type != 'LBRACE':
+        raise ValueError("Expected an object to start with '{'")
+    
+    for token in tokens:
+        if token.type == 'RBRACE':
+            return obj
+        if token.type == 'STRING':
+            key = token.value.strip('"')
+            token = next(tokens, None)
+            if token is None or token.type != 'COLON':
+                raise ValueError("Expected a ':' after a key")
+            value = parse_value(tokens)
+            obj[key] = value
+            token = next(tokens, None)
+            if token is None:
+                raise ValueError("Expected '}' or ',' in object")
+            if token.type == 'RBRACE':
+                return obj
+            if token.type != 'COMMA':
+                raise ValueError("Expected a ',' after a value in object")
+            
+def parse_array(tokens):
+    arr = []
+    
+    token = next(tokens, None)
+    if token is None or token.type != 'LBRACKET':
+        raise ValueError("Expected an array to start with '['")
+    
+    for token in tokens:
+        if token.type == 'RBRACKET':
+            return arr
+        value = parse_value(tokens)
+        arr.append(value)
+        token = next(tokens, None)
+        if token is None:
+            raise ValueError("Expected ']' or ',' in array")
+        if token.type == 'RBRACKET':
+            return arr
+        if token.type != 'COMMA':
+            raise ValueError("Expected a ',' after a value in array")
 
-    token_patterns = [
-    ('LBRACE', r'{'),
-    ('RBRACE', r'}'),
-    ('LBRACKET', r'\['),
-    ('RBRACKET', r'\]'),
-    ('COLON', r':'),
-    ('COMMA', r','),
-    ('STRING', r'"([^"\\]*(\\.[^"\\]*)*)"'),
-    ('INTEGER', r'\d+'),
-    ('FLOAT', r'-?\d+\.\d+'),
-    ('BOOLEAN', r'true|false'),
-    ('NULL', r'null'),
-]
-
-# Combine the token patterns into a single regular expression
-    token_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_patterns ) 
-
-    tokens = []
-    for match in re.finditer(token_regex, json_string):
-        for name, value in match.groupdict().items():
-            if value:
-                tokens.append((name, value))
-    return tokens
-
-json_input = '{"name": "Josh", "Age": 20}'
-tokens = tokenize_json(json_input)
-print(tokens)
+def parse_value(tokens):
+    token = next(tokens, None)
+    if token is None:
+        raise ValueError("Unexpected end of input")
+    if token.type == 'STRING':
+        return token.value.strip('"') ######
+    if token.type == 'NUMBER':
+        return float(token.value) if '.' in token.value or 'e' in token.value.lower() else int(token.value)
+    if token.type == 'TRUE':
+        return True
+    if token.type == 'FALSE':
+        return False
+    if token.type == 'NULL':
+        return None
+    if token.type == 'LBRACE':
+        return parse_object(tokens)
+    if token.type == 'LBRACKET':
+        return parse_array(tokens)
+    raise ValueError(f"Unexpected token: {token.type}")
