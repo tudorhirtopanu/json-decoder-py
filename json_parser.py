06f413b20d692779e5622e7a3aa7a9fea90387fa
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-12 10:27:30 +0000

    parse list & object without consuming extra token modify parse value so it doesnt get nxt token

--- json_parser.py
+++ json_parser.py
@@ -1,15 +1,12 @@
-
 def parse_object(tokens):
 
     # Initialize empty dictionary to represent json object
     obj = {}
     
     # Get the first token from input
-    token = next(tokens, None)
+    #token = next(tokens, None)
 
     # Check if the input starts with an opening curly brace '{'
-    if token is None or token.type != 'LBRACE':
-        raise ValueError("Expected an object to start with '{'")
     
     # Iterate through the tokens to parse key-value pairs in the object
     for token in tokens:
@@ -31,11 +28,16 @@ def parse_object(tokens):
             if token is None or token.type != 'COLON':
                 raise ValueError("Expected a ':' after a key")
             
+            print("before token: " +token.value)
+            token = next(tokens, None)
+            print("after token next: "+token.value)
+            
+            if token.value == 'LBRACE':
+                print("ASKDNFKASJN")
+
             # Check if the next token is an array or a regular JSON value
-            if token.type == 'LBRACKET':
-                value = parse_array(tokens)
-            else:
-                value = parse_value(tokens)
+
+            value = parse_value(token,tokens)
 
             # Add the key-value pair to the dictionary
             obj[key] = value
@@ -59,57 +61,14 @@ def parse_object(tokens):
 def parse_array(tokens):
     # Initialize an empty list to represent the JSON array
     arr = []
-    
-    # Get the first token from input
-    token = next(tokens, None)
 
-    print("array entered "+token.type)
-    
-    print("stop 1 " + token.type)
-
-    # TODO Error if no comma
-    # TODO Incorporate rest of function
-
-    if token.type == 'RBRACKET':
-        return arr  # Return an empty array if the array is empty
-
-    while token:
-        if token.type != None :
-            # Pass the current token to parse_value for parsing
-
-            if token.type == 'COMMA':
-                token = next(tokens, None)
-                if token.type == 'RBRACKET':
-                    raise ValueError("Object expected to follow after ','")
-
-            value = parse_value(iter([token]))  # Pass the token in an iterator
-
-            # Append the parsed value to the array
-            arr.append(value)
-
-        token = next(tokens, None)
-
-        if token is None:
-            raise ValueError("Expected ']' in array")
-
-        if token.type == 'RBRACKET':
-            print("Array End")
-            return arr  # Return the array when ']' is encountered
-        
-        
-
-     # Check if the input starts with an opening square bracket '['
-    
-     # Iterate through the tokens to parse elements in the array
+    # Iterate through the tokens to parse elements in the array
     for token in tokens:
-        print("stop 2 "+token.type)
-        # Check if the current token is the closing square bracket ']'
         if token.type == 'RBRACKET':
-            print("stop 3 "+token.type)
             return arr
-        
+
         # Parse the value associated with the key (as in JSON arrays, there are no keys)
-        value = parse_value(tokens)
+        value = parse_value(token, tokens)
 
         # Add the parsed value to the array
         arr.append(value)
@@ -117,22 +76,23 @@ def parse_array(tokens):
         # Get the next token (expecting either ']' or ',')
         token = next(tokens, None)
 
-        # Token isn't ']' ...
+        # Token isn't ']'
         if token is None:
             raise ValueError("Expected ']' or ',' in array")
-        
+
         # Check if the next token is ']' (indicating the end of the array)
         if token.type == 'RBRACKET':
             return arr
-        
-        #  Check if the next token is a comma ',' to separate elements in the array
+
+        # Check if the next token is a comma ',' to separate elements in the array
         if token.type != 'COMMA':
             raise ValueError("Expected a ',' after a value in array")
 
-def parse_value(tokens):
+
+def parse_value(token,tokens):
 
     # Get the next token from the input
-    token = next(tokens, None)
+    #token = next(tokens, None)
 
     print("value "+ token.value)
 
@@ -163,11 +123,12 @@ def parse_value(tokens):
     
     # If the token is a left curly brace '{', parse an object
     if token.type == 'LBRACE':
+        print("object found")
         return parse_object(tokens)
     
     # If the token is a left square bracket '[', parse an array
     if token.type == 'LBRACKET':
-        print("Array found")
+        print("list found")
         return parse_array(tokens)
     
     # If none of the expected token types are found, raise an error
