a619cbc3103d620f47387390b254f349f459abfd
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-13 19:46:57 +0000

    store local dummy json data in a seperate file

---
+++ dummy_json.py
@@ -0,0 +1,23 @@
+# Example JSON input
+json_input = '''
+[{
+    "name": "John",
+    "age": 30,
+    "isMarried": true,
+    "cars":[1, "A", "3"],
+    "salary":1000,
+    "object":{
+    "test":true
+    }
+},
+{
+    "name": "Sam",
+    "age": 40,
+    "isMarried": true,
+    "cars":[1, "A", "3"],
+    "salary":1000,
+    "object":{
+    "test":false
+    }
+}]
+'''
