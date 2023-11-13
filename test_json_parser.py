13c21cc4b8290f87b25c29700a55141045949bf0
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-13 20:04:52 +0000

    parse data from an api

--- test_json_parser.py
+++ test_json_parser.py
@@ -1,10 +1,20 @@
 from json_parser import parse_value
 from json_lexer import lexer
 from dummy_json import json_input
+import requests
+
+api_url = 'https://jsonplaceholder.typicode.com/posts/1/comments'
+response = requests.get(api_url)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    # Access the content of the response as a string
+    data_as_string = response.text
+else:
+    print(f"Failed to fetch data. Status code: {response.status_code}")
 
 # Create a generator of tokens using the lexer (json_lexer.tokens)
-
-lexer.input(json_input)
+lexer.input(data_as_string)
 tokens = list(lexer)
 
 # Call the parsing functions to parse the JSON
@@ -12,3 +22,8 @@ parsed_json = parse_value(tokens[0], iter(tokens[1:]))  # Assuming you have a pa
 
 # Now 'parsed_json' contains a Python data structure representing the JSON input
 print(parsed_json)
+
+object = parsed_json[1]
+object_email = object["email"]
+
+print(object_email)
